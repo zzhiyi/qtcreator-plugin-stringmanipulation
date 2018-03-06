268ac27ef14831659a88c2ce18be6947d41c0fe9
Zhiyi Zhang <zzhang@codeweavers.com>
2018-03-06 17:12:47 +0800

    Support escape chracters and encoding Unicode

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # qtcreator-plugin-stringmanipulation
-A String Manipulation Plugin For Qt Creator
+A String Manipulation Plugin For Qt Creator (Usefull in wine development)
 
 ## Feature:  
 * Selected Text to Char Array:   
-abcd -> {'a','b','c','d',0} (Usefull in wine development)
+abcd -> {'a','b','c','d',0}
+'    -> {'\'',0}
+中文 -> {0x4e2d,0x6587,0}
--- src/stringmanipulationcore.cpp
+++ src/stringmanipulationcore.cpp
@@ -8,13 +8,45 @@ StringManipulationCore::StringManipulationCore(QObject *parent) : QObject(parent
 
 }
 
+static QString escape(const QChar &c)
+{
+    switch(c.unicode())
+    {
+        case 0x07: return "\\a"; // Alert(Beep, Bell)
+        case 0x08: return "\\b"; // Backspace
+        case 0x09: return "\\t"; // Horizontal Tab
+        case 0x0C: return "\\f"; // Formfeed
+        case 0x0A: return "\\n"; // Newline(Line feed)
+        case 0x0B: return "\\v"; // Vertical Tab
+        case 0x0D: return "\\r"; // Carriage Return
+        case 0x22: return "\\\""; // Double quotation mark
+        case 0x27: return "\\\'"; // Single quotation mark
+        case 0x3F: return "\\?"; // Question mark
+        case 0x5C: return "\\\\"; // Backslash
+        default:   return c;    // No need to escape
+    }
+}
+
+static QString toUnicodeHex(const QChar &c)
+{
+    return "0x"+QString::number(c.unicode(), 16);
+}
+
 static QString stringToCharArrayConverter(const QString &src)
 {
     QString result;
     result = "{";
 
     for (int i = 0; i < src.size(); i++) {
-        result += "'" + src[i] + "',";
+        // ASCII
+        if(src[i].unicode()<=0xFF)
+        {
+            result += "'" + escape(src[i]) + "',";
+        } 
+        // Unicode Characters
+        else {
+            result += toUnicodeHex(src[i]) + ",";
+        }
     }
     result += "0}";
     return result;
