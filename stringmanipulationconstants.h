b15c94bb522f2399bd54066d7b0a60176dd1f50a
Zhiyi Zhang <zzhang@codeweavers.com>
2024-12-05 22:43:40 +0800

    Support Qt Creator 15.
    Qt Creator 15 seems to need the plugin to be in installation folder such as
    /home/eric/bin/Qt/Tools/QtCreator/lib/qtcreator/plugins/

--- stringmanipulationconstants.h
+++ stringmanipulationconstants.h
@@ -1,18 +1,20 @@
 #ifndef STRINGMANIPULATIONCONSTANTS_H
 #define STRINGMANIPULATIONCONSTANTS_H
 
+#include <utils/id.h>
+
 namespace StringManipulation {
 namespace Constants {
 
-const char SEARCH_IN_GOOGLE[] = "StringManipulation.SearchInGoogle";
-const char SELECTED_STRING_TO_CHAR_ARRAY_ACTION_ID[] = "StringManipulation.SelectedStringToCharArray";
-const char STRIP_SPACE_IN_SELECTED_STRING[] = "StringManipulation.StripSpaceInSelectedString";
-const char ADD_BREAK_POINT[] = "StringManipulation.AddBreakPoint";
-const char ADD_RETURN_ADDRESS[] = "StringManipulation.AddReturnAddress";
-const char SORT_DECLARATION_BY_LENGTH[] = "StringManipulation.SortDeclarationByLength";
-const char PASTE_DIFF[] = "StringManipulation.PasteDiff";
-const char MENU_ID[] = "StringManipulation.Menu";
-const char CONTEXT_MENU_ID[] = "StringManipulation.ContextMenu";
+const Utils::Id SEARCH_IN_GOOGLE { "StringManipulation.SearchInGoogle"};
+const Utils::Id SELECTED_STRING_TO_CHAR_ARRAY_ACTION_ID {"StringManipulation.SelectedStringToCharArray"};
+const Utils::Id STRIP_SPACE_IN_SELECTED_STRING {"StringManipulation.StripSpaceInSelectedString"};
+const Utils::Id ADD_BREAK_POINT {"StringManipulation.AddBreakPoint"};
+const Utils::Id ADD_RETURN_ADDRESS {"StringManipulation.AddReturnAddress"};
+const Utils::Id SORT_DECLARATION_BY_LENGTH {"StringManipulation.SortDeclarationByLength"};
+const Utils::Id PASTE_DIFF {"StringManipulation.PasteDiff"};
+const Utils::Id MENU_ID {"StringManipulation.Menu"};
+const Utils::Id CONTEXT_MENU_ID {"StringManipulation.ContextMenu"};
 
 } // namespace Constants
 } // namespace StringManipulation
